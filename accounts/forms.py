18f4e9ade49bee59e1fd58f9ec29ac56987fbf27
rms82 <rdam9161@gmail.com>
2022-10-09 15:00:54 +0330

    add password change

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,7 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age', )
+        fields = UserCreationForm.Meta.fields + ('email', 'age', )
 
 
 class CustomUserChangeForm(UserChangeForm):
