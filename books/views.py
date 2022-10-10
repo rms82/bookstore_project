962f613c691b2a02d77d7c0132d1f479f992f02b
rms82 <rdam9161@gmail.com>
2022-10-10 22:03:46 +0330

    add new book view

--- books/views.py
+++ books/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from django.views import generic
 
 from .models import Book
-
+from .forms import BookForm
 
 # Create your views here.
 class BookListView(generic.ListView):
     template_name = 'books/book_list.html'
     model = Book
     context_object_name = 'books'
+
+
+class BookDetailView(generic.DetailView):
+    template_name = 'books/book_detail.html'
+    model = Book
+
+
+class BookCreateView(generic.CreateView):
+    form_class = BookForm
+    template_name = 'books/book_new.html'
