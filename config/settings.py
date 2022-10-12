a1d9f95470bd8b9c46c11e6c6e56af9a1d2a782e
rms82 <rdam9161@gmail.com>
2022-10-12 18:23:33 +0330

    add static files

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -135,3 +135,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # email
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static')), ]
